8837d550c0106165c9444a20ddbcaf43b7dce5f7
manthanbytes24 <Manthanprajapati2006@gmail.com>
2026-03-05 10:22:43 +0530

    Delete FaceAttend_v8 directory

--- FaceAttend_v8/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "FaceAttend Pro V7 - Starting..."
-pip3 install flask -q 2>/dev/null
-sleep 0.5
-open http://localhost:5000 2>/dev/null || xdg-open http://localhost:5000 2>/dev/null &
-python3 app.py
