4286715cae00e6f942dbeb9101cc9d31216e9901
manthanbytes24 <Manthanprajapati2006@gmail.com>
2026-04-22 15:56:09 +0530

    Delete AttendXv3 directory

--- AttendXv3/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pip3 install flask -q 2>/dev/null || pip install flask -q 2>/dev/null
-(sleep 1 && open http://localhost:5000 2>/dev/null || xdg-open http://localhost:5000 2>/dev/null) &
-python3 app.py
